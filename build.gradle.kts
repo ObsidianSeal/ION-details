d098e23fc6180b15ed1f0481e8183406add3d94f
Obsidian_Seal <71528049+ObsidianSeal@users.noreply.github.com>
2026-06-12 18:40:17 -0400

    Update build.gradle.kts ??????

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
+}
